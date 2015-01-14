9e8af2094fe7717564cde22372030dc5cd01507b
oscarcenteno-colaborador <c3682510@trbvm.com>
2015-01-14 02:10:49 +0000

    Proyectos para iniciar el ejemplo de Fibonacci con tdd

---
+++ Demos Profesor/TDD/Intro/Fibonacci/Calculator.vb
@@ -0,0 +1,7 @@
+﻿Public Class Calculator
+
+    Public Function Fib(n As Integer) As Integer
+        Throw New NotImplementedException
+    End Function
+
+End Class
---
+++ Demos Profesor/TDD/Intro/FibonacciUnitTests/FibonaccitTests.vb
@@ -0,0 +1,10 @@
+﻿Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Imports Fibonacci
+
+<TestClass()> Public Class FibonaccitTests
+
+    <TestMethod()> Public Sub TestMethod1()
+    End Sub
+
+End Class
